f47dbd3b0e313b19ff0fcc46be6eff15419ba5a8
web3ten0 <web3ten0@gmail.com>
2022-05-02 01:41:17 +0900

    added fetch users

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,12 +1,34 @@
-<template>
-  <div>
-    <div>Users Index</div>
-    <p>API URL {{ $config.apiUrl }}</p>
-  </div>
-</template>
-
-<script>
+<script setup lang="ts">
 definePageMeta({
   layout: "default",
 });
+const conf = useRuntimeConfig();
+const { data } = await useFetch(`${conf.apiUrl}/users`);
+// FIXME
+const users = data.value.result;
 </script>
+
+<template>
+  <div>
+    <div>Users Index</div>
+    <p>API URL {{ conf.apiUrl }}</p>
+    <table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Message</td>
+          <td>Age</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" key="user.id">
+          <td>{{ user.id }}</td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.msg }}</td>
+          <td>{{ user.age }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
