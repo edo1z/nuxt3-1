3e31be8804ddd4e8263c620cf85227d317c58d81
web3ten0 <web3ten0@gmail.com>
2022-05-03 23:34:01 +0900

    sign inボタンに鍵アイコン表示

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -3,31 +3,17 @@
     <Head>
       <Title>Sign In</Title>
     </Head>
+
     <div
       class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
     >
       <div class="max-w-md w-full space-y-8">
         <div>
-          <img
-            class="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-            alt="Workflow"
-          />
           <h2
             class="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-white"
           >
-            Sign in to your account
+            Sign in
           </h2>
-          <p class="mt-2 text-center text-sm text-gray-600 dark:text-white">
-            Or
-            {{ " " }}
-            <a
-              href="#"
-              class="font-medium text-indigo-600 hover:text-indigo-500"
-            >
-              start your 14-day free trial
-            </a>
-          </p>
         </div>
         <form class="mt-8 space-y-6" action="#" method="POST">
           <input type="hidden" name="remember" value="true" />
@@ -58,32 +44,6 @@
             </div>
           </div>
 
-          <div class="flex items-center justify-between">
-            <div class="flex items-center">
-              <input
-                id="remember-me"
-                name="remember-me"
-                type="checkbox"
-                class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-              />
-              <label
-                for="remember-me"
-                class="ml-2 block text-sm text-gray-900 dark:text-white"
-              >
-                Remember me
-              </label>
-            </div>
-
-            <div class="text-sm">
-              <a
-                href="#"
-                class="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                Forgot your password?
-              </a>
-            </div>
-          </div>
-
           <div>
             <button
               type="submit"
@@ -104,8 +64,6 @@
   </div>
 </template>
 
-<script>
-definePageMeta({
-  layout: "default",
-});
+<script setup>
+import { LockClosedIcon } from "@heroicons/vue/outline";
 </script>
